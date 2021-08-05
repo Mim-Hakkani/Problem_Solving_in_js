d81a4db8cecac508a3774f08aa2f46e69ea89ded
Hakkani Mim <mimhakkani4g@gmail.com>
2021-08-05 11:53:29 +0600

    for nice comments

---
+++ area_of_math.js
@@ -0,0 +1,17 @@
+ //area of rectangle,circle,triangle
+
+            var len = 5;
+            var wid = 10;
+            var redus = 6;
+            //for circle
+            var circle = (4/3) * 3.14156 * redus * redus;
+            console.log(circle);
+            //for retacangle 
+            var rectangle = wid * len;
+            console.log(rectangle);
+            
+            //for traangle
+            var triangle = len*wid*redus;
+            console.log(triangle);
+
+    

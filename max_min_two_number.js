bcf45ed0ca772b4af21fab9f18d98a97c62a9908
Hakkani Mim <mimhakkani4g@gmail.com>
2021-08-05 14:42:48 +0600

    maximum number clac in three number using function

--- max_min_two_number.js
+++ max_min_two_number.js
@@ -11,5 +11,25 @@
     }
  
 
+ 
+    // minimum number beteween three numbers 
 
+
+    //max min in functions
+
+    function max_min(num1,num2,num3){
+        if(num1<num2 && num1<num3){
+            console.log('minimum value is  :',num1);
+        }
+        else if(num2<num3 && num2<num1)
+        {
+            console.log('minimum value is  :',num2);
+        }
+        else{
+            console.log('minimum value is  :',num3);
+        }
+    }
+    max_min(45,49,10)
+    max_min(5,10,15);
+    max_min(25,15,175);
 

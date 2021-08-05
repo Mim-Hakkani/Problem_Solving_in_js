05d2866be75236bd5e2643b713a05b5777265991
Hakkani Mim <mimhakkani4g@gmail.com>
2021-08-05 10:13:25 +0600

    leap year

---
+++ leap_year.js
@@ -0,0 +1,12 @@
+// Leap Year in java script
+
+         var time = 1900;
+        if(time%4 ==0 && time%400==0 || time%100!=0)
+        {
+            console.log('this is yeap year');
+        }
+        else
+        {
+            console.log('Not Leap year ');
+        }
+        

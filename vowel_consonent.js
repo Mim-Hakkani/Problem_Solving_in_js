3dd222758eb30c38b91be4697843ab2d5aea4f58
Hakkani Mim <mimhakkani4g@gmail.com>
2021-08-05 11:42:34 +0600

    vowel consonent

---
+++ vowel_consonent.js
@@ -0,0 +1,12 @@
+//Check The vowel Or consonent
+
+      
+            var charter = 'p';
+            if(charter=='a' || charter=='e'||charter=='i'||charter=='o'||charter=='u')
+            {
+                console.log('Hi ! This is the Vowel Number ');
+            }
+            else{
+                console.log('Thsi is  the consonen Numebr');
+            }
+       

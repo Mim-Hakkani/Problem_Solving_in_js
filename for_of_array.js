1ef132ca790c071d3831735ac77f1e21d5f06041
Hakkani Mim <mimhakkani4g@gmail.com>
2021-08-08 15:35:53 +0600

    example in for of loop

--- for_of_array.js
+++ for_of_array.js
@@ -11,8 +11,17 @@ for (const c_id of id) {
 }
 
 
+//for array 
 
 let tomar =['cse',5425,'ice']
 for (const amaser of tomar) {
     console.log(amaser);
+}
+
+//for string 
+
+let name = "my name is hakkani"
+for (const me of name)
+{
+    console.log(me);
 }

4b1b18b45ae78bd311c8c964f22266fb29c943c5
Hakkani Mim <mimhakkani4g@gmail.com>
2021-08-08 12:17:06 +0600

    count and divsile odd  number in 7 to 19

---
+++ odd_number_7_to_19.js
@@ -0,0 +1,15 @@
+let count = 0;
+for (let num = 7; num <=19; num+=2){
+   
+    if(num%3 ==0 && num%5==0)
+    {
+        console.log('It is divisible in 3 and 5');
+    }
+    else
+    {
+        console.log('Odd Numer is : ',num);
+    }
+    count++
+}
+  
+console.log('total number is :',count++);

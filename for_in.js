736d93be17fcdab15cba503ce2c595f1f1e171cc
Hakkani Mim <mimhakkani4g@gmail.com>
2021-08-08 15:53:06 +0600

    object all playing in js

--- for_in.js
+++ for_in.js
@@ -1,9 +1,10 @@
-let amar = {
+var amar = {
     name :'cse',
     roll :12,
     deptt :'eee'
 }
 
 for (const pust in amar) {
-    console.log(pust);
+    console.log(pust); 
 }
+ 
---
+++ object.js
@@ -0,0 +1,21 @@
+
+
+const person = {
+    firstname: "John",
+    lastname: " Doe",
+    age: 50,
+    eyecolor: "blue"
+  };
+//all value in object
+  console.log(person);
+
+  // show object indevidual value 
+  console.log(person.eyecolor);
+
+  //update name or properties
+  
+  person.firstname ="Golam Hakkami"
+     
+   //concate two properties and show
+   const total = person.firstname + person.lastname
+   console.log(total);

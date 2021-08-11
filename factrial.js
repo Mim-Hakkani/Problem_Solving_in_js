b3af537f2442559591f3b11e607adaab5e7e0b7f
Hakkani Mim <mimhakkani4g@gmail.com>
2021-08-11 09:15:03 +0600

    factiral number usnig function and for loop

---
+++ factrial.js
@@ -0,0 +1,20 @@
+// iterative way 
+
+let fact =1;
+for(i=1;i<=5;i++)
+{
+    fact =fact*i;
+}
+console.log(fact);
+
+//using function
+
+function factirial(num){
+    for(i=1;i<=num;i++)
+    {
+        return fact =fact*i;
+    } 
+}
+console.log(factirial(5))
+
+

f9c67bf5fd5859dc3551dfdd37235cdf669bd746
Hakkani Mim <mimhakkani4g@gmail.com>
2021-08-11 08:38:28 +0600

    decreamentig loop with same anawer

--- normal_loop.js
+++ normal_loop.js
@@ -1,5 +1,5 @@
 //incrementing loop ...see series and its summation
-sum =0;
+let sum =0;
 for(let i=0;i<=6;i++)
 {
     sum=sum+i;
@@ -7,3 +7,13 @@ for(let i=0;i<=6;i++)
     console.log(i);
 }
 console.log('Total summation is : ',sum);
+
+// same loop is working for decreamenting
+
+let summtion =0;
+for(let j=6; j>=1;j--)
+{
+    summtion =summtion+j;
+    console.log(j);
+}
+console.log(summtion);
